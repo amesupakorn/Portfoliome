2f05803f83c11822f36a4ccab79986b9bf287282
SupakornThongaerd <supakorn642@gmail.com>
2025-02-23 01:02:29 +0700

    update work

--- portfolio/src/components/skill/skill.tsx
+++ portfolio/src/components/skill/skill.tsx
@@ -1,35 +1,8 @@
 import { useState, useEffect } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa";
+import tools from "../../data/tools"; // นำเข้าข้อมูล tools จากไฟล์แยก
 
 const Skill = () => {
-  const tools = [
-    { name: "JavaScript", icon: "/logo/javascript.png", description: "Interaction" },
-    { name: "TypeScript", icon: "/logo/typescript.png", description: "Interaction" },
-
-    { name: "React", icon: "/logo/react.png", description: "Frontend Framework" },
-    { name: "NodeJS", icon: "/logo/nodejs.webp", description: "Backend Framework" },
-    { name: "NextJS", icon: "/logo/nextjs.png", description: "Full Stack Framework" },
-    { name: "Django", icon: "/logo/django.jpg", description: "Full Stack Framework" },
-
-    { name: "Python", icon: "/logo/python.png", description: "Web Development" },
-    { name: "PHP", icon: "/logo/php.jpeg", description: "Web Development" },
-
-    { name: "Java", icon: "/logo/java.png", description: "App Development" },
-
-    { name: "PostgreSQL", icon: "/logo/postgresql.png", description: "Database" },
-    { name: "MongoDB", icon: "/logo/mongodb.png", description: "Database" },
-    { name: "Prisma", icon: "/logo/prisma.png", description: "Database ORM" },
-
-    { name: "CSS", icon: "/logo/css.png", description: "User Interface" },
-    { name: "TailwindCSS", icon: "/logo/tailwind.png", description: "User Interface" },
-    { name: "Bootstrap", icon: "/logo/bootstrap.png", description: "User Interface" },
-
-    { name: "Docker", icon: "/logo/docker.jpg", description: "Containerization" },
-    { name: "Jenkins", icon: "/logo/jenkins.png", description: "CI/CD Automation" },
-    { name: "Postman", icon: "/logo/postman.png", description: "API Testing" },
-
-    { name: "Figma", icon: "/logo/figma.png", description: "Design tool" },
-  ];
 
   const [showAll, setShowAll] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
---
+++ portfolio/src/data/work.ts
@@ -0,0 +1,78 @@
+
+const projects = [
+    {
+      id: 1,
+
+      title: "MoviGo",
+      description:
+        "MoviGo is a modern movie seat booking system built with Next.js and TMDB API, allowing users to browse movies, check showtimes, and book seats seamlessly. It features real-time seat selection, a responsive UI, and dynamic movie details powered by TMDB",
+      link: "https://github.com/amesupakorn/MoviGo",
+      image: "/work/movie.png",
+      tags: ["NextJs", "Prisma", "TailwindCSS", "PostgreSQL", "TMDB API"],
+      rowSpan: "row-span-1",
+    },
+    {
+      id: 2,
+      title: "WellCareConnect",
+      description:
+        "Chatbot Solutions for Preliminary Disease Assessment and Health Guidance powered by Google Cloud for scalable infrastructure. Docker containers streamline deployment, while Jenkins automates continuous integration and delivery, maintaining platform stability.",
+      link: "https://github.com/amesupakorn/WellCareConnect",
+      image: "/work/group2.png",
+      tags: [
+        "Django",
+        "TailwindCSS",
+        "Dialogflow",
+        "Google Cloud",
+        "Docker",
+        "Jenkins",
+        "PostgreSQL",
+      ],
+      rowSpan: "row-span-1",
+    },
+    {
+      id: 3,
+      title: "SuKaPab",
+      description:
+        "A one-stop e-commerce platform offering high-quality health products, from supplements to fitness gear, designed to support your journey to a healthier lifestyle with ease and confidence.",
+      link: "https://github.com/amesupakorn/Talad-Sukkapab",
+      image: "/work/group5.png",
+      tags: ["React", "NodeJS", "Prisma", "TailwindCSS", "PostgreSQL"],
+      rowSpan: "row-span-1",
+    },
+    {
+      id: 4,
+      title: "OhYummy",
+      description:
+        "An all-in-one restaurant management website that integrates backend kitchen operations and frontend customer-facing solutions.",
+      link: "https://github.com/amesupakorn/OhYummy",
+      image: "/work/group4.png",
+      tags: ["PHP", "Bootstrap", "MariaDB"],
+      rowSpan: "row-span-1",
+    },
+    {
+      id: 5,
+      title: "HyGeas Hotel",
+      description:
+        "A comprehensive hotel management system integrating frontend guest experience and backend hotel operations.",
+      link: "https://github.com/amesupakorn/HYGEAS_HOTEL-web-",
+      image: "/work/hygeas.png",
+      tags: ["PHP", "Bootstrap", "MariaDB"],
+      rowSpan: "row-span-1",
+    },
+    {
+        id: 6,
+        title: "Teammusic",
+        description:
+          "Website Development for Audio Streaming Systems on Amazon Web Services (AWS)",
+        link: "https://github.com/amesupakorn/TM_Teammusic",
+        image: "/work/tm.png",
+        tags: ["Django", "TailwindCSS", "Amazon Web Services", "PostgreSQL"],
+        rowSpan: "row-span-1",
+      },
+
+
+    
+
+  ];
+
+  export default projects;
